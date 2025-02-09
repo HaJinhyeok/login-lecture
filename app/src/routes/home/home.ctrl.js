a5beb2a9108159a61d0ffd7688ed8b8775036f13
HaJinhyeok <hjhmoon61@naver.com>
2025-02-10 01:52:26 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,8 @@
 "use strict";
 
+const User = require("../../models/User");
 // 파일 위치 잘 보고 상위 폴더 이동 및 하위 폴더 접근 잘 해주기!
-const UserStorage = require("../../models/UserStorage");
+// const UserStorage = require("../../models/UserStorage");
 
 // output이라는 object로 묶어주기
 const output = {
@@ -15,26 +16,30 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id", "psword");
-
-        const response = {};
-        // users의 id에 현재 입력된 id값이 있으면
-        if (users.id.includes(id)) {
-            // 그 인덱스를 가져오고
-            const idx = users.id.indexOf(id);
-            // users의 psword에서 해당 인덱스에 해당하는 비밀번호 또한 현재 입력된 psword 값과 일치하면
-            if (users.psword[idx] === psword) {
-                response.success = true;
-                // success: true라는 object를 json형태로 만들어 프론트엔드로 응답해준다
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
+
+        // const id = req.body.id,
+        //     psword = req.body.psword;
+
+        // const users = UserStorage.getUsers("id", "psword");
+
+        // const response = {};
+        // // users의 id에 현재 입력된 id값이 있으면
+        // if (users.id.includes(id)) {
+        //     // 그 인덱스를 가져오고
+        //     const idx = users.id.indexOf(id);
+        //     // users의 psword에서 해당 인덱스에 해당하는 비밀번호 또한 현재 입력된 psword 값과 일치하면
+        //     if (users.psword[idx] === psword) {
+        //         response.success = true;
+        //         // success: true라는 object를 json형태로 만들어 프론트엔드로 응답해준다
+        //         return res.json(response);
+        //     }
+        // }
+        // response.success = false;
+        // response.msg = "로그인에 실패하셨습니다.";
+        // return res.json(response);
     },
 };
 
