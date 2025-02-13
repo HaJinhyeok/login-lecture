ecf78b7855c43ac9829008a27c7f09bb6fe9c5ea
HaJinhyeok <hjhmoon61@naver.com>
2025-02-13 14:12:10 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -11,6 +11,7 @@ router.get("/", ctrl.output.home);
 
 // login이라는 경로 전달 시
 router.get("/login", ctrl.output.login); //=> 실제 기능을 하는 뒤 함수 부분을 controller라 칭함
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login); // 로그인 기능을 받아와 처리하는 기능 부분
 
 // 외부파일에서 사용할 수 있도록 던져주기
