bacb96d5c17f0033af42942843e6cfa32781d3ad
HaJinhyeok <hjhmoon61@naver.com>
2025-02-13 14:29:13 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -3,19 +3,25 @@
 // DOM: document object model
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     password = document.querySelector("#password"),
-    loginBtn = document.querySelector("#button");
+    confirmPassword = document.querySelector("#confirm-password"),
+    registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click", login); // click이라는 이벤트 발생 여부를 확인하고 login 함수 실행
 
-function login() {
+registerBtn.addEventListener("click", register); // click이라는 이벤트 발생 여부를 확인하고 register 함수 실행
+
+function register() {
     const req = {
         id: id.value,
+        name: name.value,
         psword: password.value,
+        confirmPsword: confirmPassword.value,
     };
+
     // object 값을 json 문자열로 바꿔 감싸줌
 
-    fetch("/login", {
+    fetch("/register", {
         method: "POST", // POST라는 http 메서드로 전달해 주어야함 ??? RESTful API 개념
         headers: {
             "Content-Type": "application/json",
@@ -26,13 +32,13 @@ function login() {
         .then((res) => {
             if (res.success) {
                 // 위치(location)을 루트로 바꿔줌
-                location.href = "/";
+                location.href = "/login";
             } else {
                 alert(res.msg);
             }
         })
         .catch((err) => {
-            console.error(new Error("로그인 중 에러 발생"));
+            console.error(new Error("회원가입 중 에러 발생"));
         });
     // .then((res) => console.log(res));
     //.then(console.log); // 해당 값을 받아와 그대로 사용할 때 이처럼 생략 가능
