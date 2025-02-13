9ee275924e53a8effa6654073d5783660f3c32d2
HaJinhyeok <hjhmoon61@naver.com>
2025-02-13 16:32:46 +0900

    깃 버전 관리 6분 1초 경

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -22,28 +22,32 @@ const process = {
         const user = new User(req.body);
         const response = user.login();
         return res.json(response);
-
-        // const id = req.body.id,
-        //     psword = req.body.psword;
-
-        // const users = UserStorage.getUsers("id", "psword");
-
-        // const response = {};
-        // // users의 id에 현재 입력된 id값이 있으면
-        // if (users.id.includes(id)) {
-        //     // 그 인덱스를 가져오고
-        //     const idx = users.id.indexOf(id);
-        //     // users의 psword에서 해당 인덱스에 해당하는 비밀번호 또한 현재 입력된 psword 값과 일치하면
-        //     if (users.psword[idx] === psword) {
-        //         response.success = true;
-        //         // success: true라는 object를 json형태로 만들어 프론트엔드로 응답해준다
-        //         return res.json(response);
-        //     }
-        // }
-        // response.success = false;
-        // response.msg = "로그인에 실패하셨습니다.";
-        // return res.json(response);
     },
+    register: (req, res) => {
+        const user = new User(req.body);
+        const response = user.register();
+        return res.json(response);
+    },
+    // const id = req.body.id,
+    //     psword = req.body.psword;
+
+    // const users = UserStorage.getUsers("id", "psword");
+
+    // const response = {};
+    // // users의 id에 현재 입력된 id값이 있으면
+    // if (users.id.includes(id)) {
+    //     // 그 인덱스를 가져오고
+    //     const idx = users.id.indexOf(id);
+    //     // users의 psword에서 해당 인덱스에 해당하는 비밀번호 또한 현재 입력된 psword 값과 일치하면
+    //     if (users.psword[idx] === psword) {
+    //         response.success = true;
+    //         // success: true라는 object를 json형태로 만들어 프론트엔드로 응답해준다
+    //         return res.json(response);
+    //     }
+    // }
+    // response.success = false;
+    // response.msg = "로그인에 실패하셨습니다.";
+    // return res.json(response);
 };
 
 module.exports = {
