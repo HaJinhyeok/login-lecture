68ba6fe76a5f5dbe393b6e2db44246dd4a04dc17
HaJinhyeok <hjhmoon61@naver.com>
2025-02-19 16:23:34 +0900

    morgan 활용 로그 관리

--- app/app.js
+++ app/app.js
@@ -7,12 +7,18 @@ const bodyParser = require("body-parser");
 // 일반적으로 개발은 여러 명이 같이 하는데, 개인마다 OS(윈도우, 맥, 리눅스 등)가 다른 경우가 있고,
 // OS마다 환경 변수 등록 방식이 다르기 때문에 노드 측에서 제시한 dotenv 모듈을 사용해 동일하게 환경 변수 등록 가능해짐
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
 
 const app = express();
+dotenv.config();
+
+const accessLogStream = require("./src/config/log");
 
 // 라우팅
 const home = require("./src/routes/home");
+
+
+
 
 // 앱 세팅
 app.set("views", "./src/views");    // views라는 폴더로 뷰 설정
@@ -21,6 +27,12 @@ app.use(express.static(`${__dirname}/src/public`)); // __dirname: 현재 app.js�
 app.use(bodyParser.json());
 // url을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(morgan("tiny"));
+// app.use(morgan(":method :date[web] :status :res[content-length] - :response-time ms"));
+app.use(morgan("dev"));
+// 로그 기록할 땐 적절한 포맷을 정해서 저장할 것.
+app.use(morgan("common", { stream: accessLogStream }));
+// app.use(morgan("combined"));
 
 app.use("/", home); // use-> 미들 웨어를 등록해주는 메서드
 
